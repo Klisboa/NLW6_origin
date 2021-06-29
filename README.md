3cfc3468face926bc219447c928bb6d03558429e
Carol Lisboa <eucarolisboa@gmail.com>
2021-06-28 21:04:58 -0300

    primeiro commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# NLW6_origin
---
+++ main.js
@@ -0,0 +1,42 @@
+// DOM Document Object Model 
+/*abre e fecha o menu quando clicar no icone: hamburguer e x  */
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for(const element of toggle) {
+  element.addEventListener('click', function() {
+    nav.classList.toggle('show')
+  })
+}
+/* quando clicar em um item do menu, esconder o menu */
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+
+/* mudar o header da pagina quando der scroll */
+const header = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function() {
+  if(this.window.scrollY >= navHeight) {
+  //scroll é maior q a altura do header
+  header.classList.add('scroll')
+}else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
+
+/* TESTIMONIAL, slider, carrocel*/
+const swiper = new Swiper('.swiper-container', {
+ slidesPerView:1,
+ pagination: {
+   el: '.swiper-pagination'
+ },
+ mausewheel: true,
+ keyboard: true
+});
