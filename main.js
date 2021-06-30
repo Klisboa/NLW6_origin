b6cdc7ca79d53d49260496cb9e7bf8f72b5ac556
Carol Lisboa <eucarolisboa@gmail.com>
2021-06-30 13:20:56 -0300

    Sessão entre em contato.

--- main.js
+++ main.js
@@ -40,3 +40,20 @@ const swiper = new Swiper('.swiper-container', {
  mausewheel: true,
  keyboard: true
 });
+
+/*ScrollReveal: Mostrar elementos quando der scroll na página */
+const scrollReveal = ScrollReveal ({
+  origin: "top",
+  distance: "30px",
+  duration: 700,
+  reset: true
+})
+
+scrollReveal.reveal(
+  `#home .text, #home .image,
+  #about .image, #about .text,
+  #services header, #services .card,
+  #testimonials header, #testimonials .testimonials,
+  #contact .text, #contact .links`, 
+  {interval: 100}
+  )
