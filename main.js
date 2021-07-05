369b00968f74d727df488f4197960af3efde96fd
Carol Lisboa <eucarolisboa@gmail.com>
2021-07-05 20:27:23 -0300

    desenvolvimento de função botão voltar

--- main.js
+++ main.js
@@ -18,18 +18,19 @@ for (const link of links) {
 }
 
 /* mudar o header da pagina quando der scroll */
-const header = document.querySelector('#header')
-const navHeight = header.offsetHeight
 
-window.addEventListener('scroll', function() {
-  if(this.window.scrollY >= navHeight) {
+function changeHeaderWhenScroll() {
+  const header = document.querySelector('#header')
+  const navHeight = header.offsetHeight
+  if(window.scrollY >= navHeight) {
   //scroll é maior q a altura do header
   header.classList.add('scroll')
 }else {
     //menor que a altura do header
     header.classList.remove('scroll')
   }
-})
+}
+
 
 /* TESTIMONIAL, slider, carrocel*/
 const swiper = new Swiper('.swiper-container', {
@@ -54,6 +55,25 @@ scrollReveal.reveal(
   #about .image, #about .text,
   #services header, #services .card,
   #testimonials header, #testimonials .testimonials,
-  #contact .text, #contact .links`, 
+  #contact .text, #contact .links, 
+  footer .brand, footer .social
+  `,
   {interval: 100}
   )
+
+  /*BOTAO VOLTAR PARA O TOPO */
+
+  function backToTop() {
+    const backToTopButton = document.querySelector('.back-to-top')
+
+    if (window.scrollY >= 560) {
+      backToTopButton.classList.add('show')
+    } else {
+      backToTopButton.classList.remove('show')
+    }
+  }
+  /* WHEM SCROLL */
+  window.addEventListener('scroll', function() {
+  changeHeaderWhenScroll()
+  backToTop()
+})
